524b2d1771c418f01c24a94f91f070eba5741413
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-08-18 10:04:19 -0300

    Add dependencias de testes e icones, e inserindo validações de visibilidade de componentes na tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from "react";
+import { FaRegCopy } from "react-icons/fa";
 
 export default function Home() {
   const [formValues, setFormValues] = useState({
@@ -9,6 +10,9 @@ export default function Home() {
     numbers: false,
     specialSymbols: false,
   });
+
+  const [generatedPassword, setGeneratedPassword] = useState<string>('');
+  const [modal, setOpenModal] = useState<boolean>(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
@@ -67,72 +71,102 @@ export default function Home() {
       const valorSorteado = sortearValor(unionParamConsiderados);
       password += valorSorteado;
     }
-    console.log(password);
+
+    setGeneratedPassword(password);
+  }
+
+  function pasteText() {
+    navigator.clipboard.writeText(generatedPassword);
+    setOpenModal(true);
   }
 
 
   return (
     <main className="w-full h-full flex flex-col gap-[32px] items-center justify-center">
-      <section className="w-full flex flex-col items-center justify-center">
-        <form
-          title="Generate a secure Password"
-          className="w-4/12"
-        >
-          <fieldset className="w-full border rounded-2xl border-[#c7c7c7] flex flex-col items-start justify-center px-10 py-5 gap-5">
-            <legend className="font-bold text-3xl">Generate a secure Password</legend>
+      {
+        !generatedPassword && (
+          <section className="w-full flex flex-col items-center justify-center">
+            <form
+              title="Generate a secure Password"
+              className="w-4/12"
+            >
+              <fieldset className="w-full border rounded-2xl border-[#c7c7c7] flex flex-col items-start justify-center px-10 py-5 gap-5">
+                <legend className="font-bold text-3xl">Generate a secure Password</legend>
 
-            <div className="w-full flex flex-row items-center justify-start gap-2">
-              <label htmlFor="passwordLength">Password length:</label>
-              <input
-                type="number"
-                required
-                min={6}
-                max={64}
-                className="w-8/12 border rounded-2xl border-[#c7c7c7] px-2 py-1 outline-none"
-                id="passwordLength"
-                name="passwordLength"
-                placeholder="6"
-                value={formValues.passwordLength}
-                onChange={handleChange}
-              />
-            </div>
+                <div className="w-full flex flex-row items-center justify-start gap-2">
+                  <label htmlFor="passwordLength">Password length:</label>
+                  <input
+                    type="number"
+                    required
+                    min={6}
+                    max={64}
+                    className="w-8/12 border rounded-2xl border-[#c7c7c7] px-2 py-1 outline-none"
+                    id="passwordLength"
+                    name="passwordLength"
+                    placeholder="6"
+                    value={formValues.passwordLength}
+                    onChange={handleChange}
+                  />
+                </div>
 
-            <div className="w-full flex flex-row justify-start items-center gap-4">
-              <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
-                <label htmlFor="uppercaseLetters">Capital letters</label>
-                <input type="checkbox" id="uppercaseLetters" name="uppercaseLetters" checked={formValues.uppercaseLetters} onChange={handleChange} />
-              </div>
+                <div className="w-full flex flex-row justify-start items-center gap-4">
+                  <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
+                    <label htmlFor="uppercaseLetters">Capital letters</label>
+                    <input type="checkbox" id="uppercaseLetters" name="uppercaseLetters" checked={formValues.uppercaseLetters} onChange={handleChange} />
+                  </div>
 
-              <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
-                <label htmlFor="lowercaseLetters">Lowercase letters</label>
-                <input type="checkbox" id="lowercaseLetters" name="lowercaseLetters" checked={formValues.lowercaseLetters} onChange={handleChange} />
-              </div>
-            </div>
+                  <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
+                    <label htmlFor="lowercaseLetters">Lowercase letters</label>
+                    <input type="checkbox" id="lowercaseLetters" name="lowercaseLetters" checked={formValues.lowercaseLetters} onChange={handleChange} />
+                  </div>
+                </div>
 
-            <div className="w-full flex flex-row justify-start items-center gap-4">
-              <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
-                <label htmlFor="numbers">Numbers</label>
-                <input type="checkbox" id="numbers" name="numbers" checked={formValues.numbers} onChange={handleChange} />
-              </div>
+                <div className="w-full flex flex-row justify-start items-center gap-4">
+                  <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
+                    <label htmlFor="numbers">Numbers</label>
+                    <input type="checkbox" id="numbers" name="numbers" checked={formValues.numbers} onChange={handleChange} />
+                  </div>
 
-              <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
-                <label htmlFor="specialSymbols">Special symbols (!@#$%&*)</label>
-                <input type="checkbox" id="specialSymbols" name="specialSymbols" checked={formValues.specialSymbols} onChange={handleChange} />
-              </div>
-            </div>
+                  <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
+                    <label htmlFor="specialSymbols">Special symbols (!@#$%&*)</label>
+                    <input type="checkbox" id="specialSymbols" name="specialSymbols" checked={formValues.specialSymbols} onChange={handleChange} />
+                  </div>
+                </div>
 
-            <div className="w-full flex flex-col items-center justify-center">
-              <button
-                className="border rounded-2xl border-[#c7c7c7] px-6 py-2 bg-[#e61111] text-white border-none"
-                onClick={proncessarSenha}
-                type="button"
-              >
-                Generate password
+                <div className="w-full flex flex-col items-center justify-center">
+                  <button
+                    className="border rounded-2xl border-[#c7c7c7] px-6 py-2 bg-[#e61111] text-white border-none"
+                    onClick={proncessarSenha}
+                    type="button"
+                  >
+                    Generate password
+                  </button>
+                </div>
+              </fieldset>
+            </form>
+          </section>
+        )
+      }
+
+      {
+        generatedPassword && (
+          <section className="w-full flex flex-col items-center justify-center gap-4">
+            <h1 className="font-bold text-3xl">Senha gerada!</h1>
+            <div className="flex flex-row border rounded-2xl border-[#c7c7c7] px-4 py-2">
+              <input type="password" readOnly className="outline-none" value={generatedPassword.slice(0, 30)}/>
+              <button onClick={(e) => { e.preventDefault(); pasteText(); }}>
+                <FaRegCopy />
               </button>
             </div>
-          </fieldset>
-        </form>
-      </section>
+          </section>
+        )
+      }
+      {
+        modal && (
+          modal
+        )
+      }
+
     </main>
   );
 }
