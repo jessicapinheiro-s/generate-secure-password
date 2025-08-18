5fa2cee6bf71a4ee1f6b84d842f9f03d97cf8e52
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-08-17 23:18:02 -0300

    Criando formulário de requisitos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,64 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="w-full h-full flex flex-col gap-[32px] items-center justify-center">
+      <section className="w-full flex flex-col items-center justify-center">
+        <form
+          title="Generate a secure Password"
+          action="submit"
+          className="w-4/12"
+        >
+          <fieldset className="w-full border rounded-2xl border-[#c7c7c7] flex flex-col items-start justify-center px-10 py-5 gap-5">
+            <legend className="font-bold text-3xl">Generate a secure Password</legend>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            <div className="w-full flex flex-row items-center justify-start gap-2">
+              <label htmlFor="passwordLength">Password length:</label>
+              <input
+                type="number"
+                required
+                min={6}
+                max={64}
+                className="w-8/12 border rounded-2xl border-[#c7c7c7] px-2 py-1"
+                id="passwordLength"
+                name="passwordLength"
+                placeholder="6"
+              />
+            </div>
+
+            <div className="w-full flex flex-row justify-start items-center gap-4">
+              <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
+                <label htmlFor="uppercaseLetters">Capital letters</label>
+                <input type="checkbox" id="uppercaseLetters" name="uppercaseLetters" />
+              </div>
+
+              <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
+                <label htmlFor="lowercaseLetters">Lowercase letters</label>
+                <input type="checkbox" id="lowercaseLetters" name="lowercaseLetters" />
+              </div>
+            </div>
+
+            <div className="w-full flex flex-row justify-start items-center gap-4">
+              <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
+                <label htmlFor="numbers">Numbers</label>
+                <input type="checkbox" id="numbers" name="numbers" />
+              </div>
+
+              <div className="flex flex-row-reverse items-center justify-end gap-2 w-6/12">
+                <label htmlFor="specialSymbols">Special symbols (!@#$%&*)</label>
+                <input type="checkbox" id="specialSymbols" name="specialSymbols" />
+              </div>
+            </div>
+
+            <div className="w-full flex flex-col items-center justify-center">
+              <button
+                type="submit"
+                className="border rounded-2xl border-[#c7c7c7] px-6 py-2 bg-[#e61111] text-white border-none"
+              >
+                Generate password
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </section>
+    </main>
   );
 }
