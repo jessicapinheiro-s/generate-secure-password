fda5865671a7ef0751ecb77ac2d04158a7dce3f5
Jéssica Pinheiro <jessicasilva.js1314@gmail.com>
2025-08-18 13:20:02 -0300

    Criando header component

--- src/componentes/header.tsx
+++ src/componentes/header.tsx
@@ -1,8 +1,13 @@
+import { PiPasswordBold } from "react-icons/pi";
+import { TbLockPassword } from "react-icons/tb";
+
 export default function Header () {
     return(
-        <header>
-            <div>
-                
+        <header className="flex flex-row w-full bg-white shadow-xl rounded-lg px-50 py-6  items-center justify-between gap-10">
+            <div className="flex flex-row">
+                <TbLockPassword  className="text-4xl"/>
+                <PiPasswordBold className="text-4xl"/>
+
             </div>
             <div>
                 <h1>GENERATE PASSWORD</h1>
